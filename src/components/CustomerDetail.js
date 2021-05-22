653f9242505526d3dbac93d599bdb5572db1856a
Abhay Jirati <abhayjirati@Abhays-MacBook-Air.local>
2021-05-23 00:51:52 +0530

    removing the unwanted code

--- src/components/CustomerDetail.js
+++ src/components/CustomerDetail.js
@@ -1,13 +1,9 @@
 import { useEffect, useState } from 'react';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import Avatar from '@material-ui/core/Avatar';
-import { makeStyles } from '@material-ui/core/styles';
-import KeyboardBackspaceSharp from '@material-ui/icons/KeyboardBackspaceSharp';
 import  { Link } from 'react-router-dom';
+import { Card, CardHeader, CardMedia, Avatar } from '@material-ui/core';
+import KeyboardBackspaceSharp from '@material-ui/icons/KeyboardBackspaceSharp';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 
 function CustomerDetail(props) {
 
@@ -52,16 +48,6 @@ function CustomerDetail(props) {
         media: {
             height: 0,
             paddingTop: '56.25%', // 16:9
-        },
-        expand: {
-            transform: 'rotate(0deg)',
-            marginLeft: 'auto',
-            transition: theme.transitions.create('transform', {
-                duration: theme.transitions.duration.shortest,
-            }),
-        },
-        expandOpen: {
-            transform: 'rotate(180deg)',
         },
         avatar: {
             backgroundColor: '#3f51b5',
@@ -162,60 +148,3 @@ function CustomerDetail(props) {
 }
 
 export default CustomerDetail;
-
-
-
-// import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-
-// import tileData from './tileData';
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: 'flex',
-//     flexWrap: 'wrap',
-//     justifyContent: 'space-around',
-//     overflow: 'hidden',
-//     backgroundColor: theme.palette.background.paper,
-//   },
-//   gridList: {
-//     flexWrap: 'nowrap',
-//     // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-//     transform: 'translateZ(0)',
-//   },
-//   title: {
-//     color: theme.palette.primary.light,
-//   },
-//   titleBar: {
-//     background:
-//       'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-//   },
-// }));
-
-// export default function SingleLineGridList() {
-//   const classes = useStyles();
-
-//   return (
-//     <div className={classes.root}>
-//       <GridList className={classes.gridList} cols={2.5}>
-//         {tileData.map((tile) => (
-//           <GridListTile key={tile.img}>
-//             <img src={tile.img} alt={tile.title} />
-//             <GridListTileBar
-//               title={tile.title}
-//               classes={{
-//                 root: classes.titleBar,
-//                 title: classes.title,
-//               }}
-//               actionIcon={
-//                 <IconButton aria-label={`star ${tile.title}`}>
-//                   <StarBorderIcon className={classes.title} />
-//                 </IconButton>
-//               }
-//             />
-//           </GridListTile>
-//         ))}
-//       </GridList>
-//     </div>
-//   );
-// }
